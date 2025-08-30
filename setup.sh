3cfaae81b878a4be7764426a2d0d6ca18d7a7ae8
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2025-08-30 15:37:35 +0200

    setup script

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -o errexit  # set -e -> exit on failure
+set -o pipefail # fail if any command in a pipe fails
+set -o nounset  # set -u -> exit on use of uninitialized variable
+
+stow git
+stow tmux
+stow idea
