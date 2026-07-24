381cf5482eb47338aa7e0b58728708ddda15b656
Omar Saber Talaat Abdelrahman <omar.abdelrahman.de@gmail.com>
2026-07-24 16:02:04 +0200

    feat(nvim): add autoformat-on-save toggle

--- nvim/dot-config/nvim/lua/plugins/conform.lua
+++ nvim/dot-config/nvim/lua/plugins/conform.lua
@@ -16,11 +16,32 @@ require("conform").setup({
     css = { "prettier" },
     sh = { "shfmt" },
   },
-  format_on_save = function()
+  format_on_save = function(bufnr)
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return
+    end
     return { timeout_ms = 1000, lsp_format = "fallback" }
   end,
 })
 
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    vim.b.disable_autoformat = true -- current buffer only
+  else
+    vim.g.disable_autoformat = true -- global
+  end
+end, { desc = "Disable autoformat-on-save", bang = true })
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, { desc = "Re-enable autoformat-on-save" })
+
 vim.keymap.set({ "n", "v" }, "<leader>cf", function()
   require("conform").format({ async = true, lsp_format = "fallback" })
 end, { desc = "Format" })
+
+vim.keymap.set("n", "<leader>uf", function()
+  vim.g.disable_autoformat = not vim.g.disable_autoformat
+  vim.notify("Autoformat-on-save " .. (vim.g.disable_autoformat and "disabled" or "enabled"))
+end, { desc = "Toggle autoformat-on-save" })
